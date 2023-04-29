f1145b920da2a7b8d70fc461b08deb440c23aa4e
Pherrows <yhk1492@naver.com>
2023-04-29 16:23:35 +0900

    4-29 집 복사

--- music2/js/main.js
+++ music2/js/main.js
@@ -1,22 +1,23 @@
 
-const moreBtn = document.querySelector(".more_list a");
-const hiddenItemEl = document.querySelectorAll(".hidden");
+// const moreBtn = document.querySelector(".more_list a");
+// const hiddenItemEl = document.querySelectorAll(".hidden");
 
-moreBtn.addEventListener('click', () => {
-  hiddenItemEl.forEach(item => {
-    item.classList.toggle('hidden');
-  });
+// const newElement = document.createElement('li');
+// const originalElement = document.querySelector('.main .list_item');
+// const clonedElement = originalElement.cloneNode(true); // true를 전달하여 자식 요소들도 복사
+// newElement.appendChild(clonedElement);
 
-  if (moreBtn.textContent === '닫기') {
-    moreBtn.textContent = '더 보기';
-  } else {
-    moreBtn.textContent = '닫기';
-  }
+const moreBtn = document.querySelector('.more_list a');
+moreBtn.addEventListener('click', function() {
+  // 새로운 요소 만들기
+  const newElement = document.createElement('div');
+
+  // 이전 요소 복사하여 붙여넣기
+  const originalElement = document.querySelector('.list_item');
+  const clonedElement = originalElement.cloneNode(true);
+  newElement.appendChild(clonedElement);
+
+  // 새로운 요소 문서에 삽입하기
+  const container = document.querySelector('.list');
+  container.appendChild(newElement);
 });
-
-
-
-
-let html = '<li>';
-html += '<img src>';
-html += '</li>';
