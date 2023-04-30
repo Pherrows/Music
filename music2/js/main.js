6f7b915ceb7479cd81f0c79e0d980249c0d976d9
Pherrows <yhk1492@naver.com>
2023-04-30 18:15:13 +0900

    4-30 집

--- music2/js/main.js
+++ music2/js/main.js
@@ -7,17 +7,40 @@
 // const clonedElement = originalElement.cloneNode(true); // true를 전달하여 자식 요소들도 복사
 // newElement.appendChild(clonedElement);
 
-const moreBtn = document.querySelector('.more_list a');
-moreBtn.addEventListener('click', function() {
-  // 새로운 요소 만들기
-  const newElement = document.createElement('div');
+// const moreBtn = document.querySelector('.more_list a');
+// moreBtn.addEventListener('click', function() {
+//   // 새로운 요소 만들기
+//   const newElement = document.createElement('div');
 
-  // 이전 요소 복사하여 붙여넣기
-  const originalElement = document.querySelector('.list_item');
-  const clonedElement = originalElement.cloneNode(true);
-  newElement.appendChild(clonedElement);
+//   // 이전 요소 복사하여 붙여넣기
+//   const originalElement = document.querySelector('.list_item');
+//   const clonedElement = originalElement.cloneNode(true);
+//   newElement.appendChild(clonedElement);
 
-  // 새로운 요소 문서에 삽입하기
-  const container = document.querySelector('.list');
-  container.appendChild(newElement);
+//   // 새로운 요소 문서에 삽입하기
+//   const container = document.querySelector('.list');
+//   container.appendChild(newElement);
+// });
+
+
+const container = document.querySelector('.list');
+const btnMore = document.querySelector('.more_list a');
+const items = document.querySelectorAll('.list_item');
+const itemsPerPage = 3;
+let currentPage = 1;
+
+btnMore.addEventListener('click', () => {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  for (let i = startIndex; i < endIndex && i < items.length; i++) {
+    const itemCopy = items[i].cloneNode(true);
+    container.appendChild(itemCopy);
+  }
+
+  currentPage++;
+
+  if (endIndex >= items.length) {
+    btnMore.style.display = 'none';
+  }
 });
